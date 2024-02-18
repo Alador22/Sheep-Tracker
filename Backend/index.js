03ce2a562906c6c4d4f1a9ddb66c8b3c2722ccc1
wavebreker <112630282+Alador22@users.noreply.github.com>
2024-02-18 16:10:53 +0100

    general stuff

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+//const express = require("express");
+//const mongoose = require("mongoose");
+
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("server is running");
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://alador:9fElxTe3xTMdvSL7@cluster0.dxz66lm.mongodb.net/sheeptracker?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(process.env.PORT || 5000);
+    console.log("database connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+/* .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dxz66lm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  ) */
