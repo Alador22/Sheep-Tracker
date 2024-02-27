a4d9ce157bc8bc03ecd17d015406292c099082ff
allijakobi <ali.jakk7@gmail.com>
2024-02-27 12:17:20 +0100

    ok

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.get("/", (request, response) => {
-  console.log(request);
+
   return response.status(234).send("server is running");
 });
 
---
+++ Backend/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require ('mongoose');
+
+const uniqueValidator = require ('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+
+
+    email: {type: String, required: true, unique: true },
+
+    password: {type: String, required: true, minLength: 6  },
+
+});
+
+    userSchema.plugin(uniqueValidator);
+
+    module.exports = mongoose.model('User', userSchema);
