816d8afe8e589778c1fb74b62b57617abd0785c2
Sondre234 <93455450+Sondre234@users.noreply.github.com>
2024-03-25 06:09:23 +0100

    Update Login.js
    Add login function

--- Login.js
+++ Login.js
@@ -1,6 +1,34 @@
 import './Login.css';
+import React, { useState } from 'react';
+import axios from 'axios'; 
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/user/login', {
+        email,
+        password
+      });
+
+      console.log('Login successful:', response.data);
+      // Handle successful login here 
+
+    } catch (error) {
+      console.error('Login failed:', error.response ? error.response.data.message : error.message);
+      console.log(error.response ? error.response.data.message : error.message)
+      // Handle login failure here 
+    }
+  };
   return (
 
     <div className="Main">
