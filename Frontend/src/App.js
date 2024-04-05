3f4312b3b91cabb70e94042c74d359f17320f81e
Sondre234 <sondrefmyrmel@gmail.com>
2024-04-05 09:27:27 +0200

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,7 +9,7 @@ import Registrer from './Registrer';
 function App() {
   const items = ['Marie', 'Ingrid', 'Rolf', 'Kevin', 'Leif', 'Jenny', 'Ali'];
   const [searchTerm, setSearchTerm] = useState('');
-
+  
   const filteredItems = items.filter(item =>
     item.toLowerCase().includes(searchTerm.toLowerCase())
   );
